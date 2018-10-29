7668f3e6f3819b88a2ae59985be8253204b542a0
Conny Andersson <conny.andersson@scila.se>
2018-10-29 08:28:31 +0100

    Initial project add

---
+++ src/main/java/com/bolero/sf5b2gpetclinic/Sf5b2gPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.bolero.sf5b2gpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf5b2gPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sf5b2gPetClinicApplication.class, args);
+    }
+}
---
+++ src/test/java/com/bolero/sf5b2gpetclinic/Sf5b2gPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bolero.sf5b2gpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Sf5b2gPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
