4605d5e699e37f597d1554cd6178ba3047be6a7c
Conny Andersson <conny.andersson@scila.se>
2018-11-07 11:09:01 +0100

    Add Pet Clinic Index Page and Controller. Closes #14.

---
+++ pet-clinic-web/src/main/java/se/bolero/sf5b2gpetclinic/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package se.bolero.sf5b2gpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+
+}
