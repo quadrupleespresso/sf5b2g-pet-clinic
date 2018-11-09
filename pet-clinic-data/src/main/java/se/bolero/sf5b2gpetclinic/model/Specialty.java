c139d0bdd1d9bfb381bc96f28ea39eb90c8c3e5f
Conny Andersson <conny.andersson@scila.se>
2018-11-09 15:24:26 +0100

    Create Vet Specialty Entity, Associate to Vet. Closes #30.

---
+++ pet-clinic-data/src/main/java/se/bolero/sf5b2gpetclinic/model/Specialty.java
@@ -0,0 +1,14 @@
+package se.bolero.sf5b2gpetclinic.model;
+
+public class Specialty extends BaseEntity {
+
+    private String specialty;
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+}
