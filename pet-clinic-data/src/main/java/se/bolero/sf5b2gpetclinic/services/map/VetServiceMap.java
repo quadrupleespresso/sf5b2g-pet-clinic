60dd92d65a7bfba21e5ba958b7631b3e9cb3798a
Conny Andersson <conny.andersson@scila.se>
2018-11-09 17:38:06 +0100

    Craete Specialties, add to vets on startup (Bootstrap). #37

--- pet-clinic-data/src/main/java/se/bolero/sf5b2gpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/se/bolero/sf5b2gpetclinic/services/map/VetServiceMap.java
@@ -1,13 +1,22 @@
 package se.bolero.sf5b2gpetclinic.services.map;
 
 import org.springframework.stereotype.Service;
+import se.bolero.sf5b2gpetclinic.model.Specialty;
 import se.bolero.sf5b2gpetclinic.model.Vet;
+import se.bolero.sf5b2gpetclinic.services.SpecialtiesService;
 import se.bolero.sf5b2gpetclinic.services.VetService;
 
 import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtiesService specialtiesService;
+
+    public VetServiceMap(SpecialtiesService specialtiesService) {
+        this.specialtiesService = specialtiesService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -20,6 +29,16 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+
+        if(object.getSpecialties().size() > 0) {
+            object.getSpecialties().forEach(specialty -> {
+                if(specialty.getId() == null) {
+                    Specialty savedSpecialty = specialtiesService.save(specialty);
+                    specialty.setId(savedSpecialty.getId());
+                }
+            });
+        }
+
         return super.save(object);
     }
 
