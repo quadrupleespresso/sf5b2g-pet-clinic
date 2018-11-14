cadab7308aeff84c37b99b36f20e7af66f041131
Conny Andersson <conny.andersson@scila.se>
2018-11-14 09:37:29 +0100

    Create Visit JPA Service. Closes #51.

---
+++ pet-clinic-data/src/main/java/se/bolero/sf5b2gpetclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,49 @@
+package se.bolero.sf5b2gpetclinic.services.springdatajpa;
+
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import se.bolero.sf5b2gpetclinic.model.Visit;
+import se.bolero.sf5b2gpetclinic.repositories.VisitRepository;
+import se.bolero.sf5b2gpetclinic.services.VisitService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        HashSet<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
